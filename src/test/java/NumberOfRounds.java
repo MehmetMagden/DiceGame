2fd958a34e734a947c6bbf36c4084f12a74bdbc6
Mehmet Magden <yasamtr@gmail.com>
2022-09-09 10:20:30 +0300

    minor duzeltme 4

--- src/test/java/NumberOfRounds.java
+++ src/test/java/NumberOfRounds.java
@@ -16,17 +16,14 @@ public class NumberOfRounds {
             // giving message to inform the user
             System.out.print("Enter Number of Rounds: ");
 
-            // Receiving the number of the rounds from the user
+            // Receiving the number of the rounds from the user with try catch
             numberOfRounds= TryCatch.tryCatch();
 
         }
+            //if the number between [1-99], the game will ask again to enter a number
         while (numberOfRounds<1||numberOfRounds>99);
 
-
-
-
-
-
+            // will return number of rounds
         return numberOfRounds;
 
 
--- src/test/java/TryCatch.java
+++ src/test/java/TryCatch.java
@@ -10,12 +10,15 @@ public class TryCatch {
         // Creating scan object to receive number of rounds from the user
         Scanner scan = new Scanner(System.in);
 
+        //if user enters a letter or unvalid sign, game will ask user to enter a number
         do {
             try {
+                //gets a data from user
                 numberOfRounds = scan.nextInt();
                 scan.nextLine();
                 break;
 
+                // if the data is not a letter, the game will ask again to enter a number
             } catch (Exception e) {
                 System.out.print("Please enter a number: ");
                 tryCatch();
@@ -23,7 +26,9 @@ public class TryCatch {
             }
         } while (false);
 
+
+        // tryCatch will return a valid int
         return numberOfRounds;
 
-}
+    }
 }
